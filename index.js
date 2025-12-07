49958a94cf5ac6e6023b440529f3d47469d97a61
calton momaya <nyankieyacalton@gmail.com>
2025-12-07 16:33:23 +0300

    analyzing data modules

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
+require('datejs'); //
 
+function combineUsers(...args) {
+  //  return object
+  const combinedObject = {
+    users: []
+  };
 
+  // merge & loop
+  for (const arr of args) {
+    combinedObject.users.push(...arr);
+  }
+
+  //add mergedate with datejs
+  const today = new Date();
+  combinedObject.merge_date = today.toString("M/d/yyyy");
+
+  // returns
+  return combinedObject;
+}
 
 module.exports = {
-  ...(typeof combineUsers !== 'undefined' && { combineUsers })
+  combineUsers
 };
